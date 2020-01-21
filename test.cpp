81a88e5fc5a988e75cf0bf71bc2303b5be69a889
xieyinjie@hotmail.com <xieyinjie@hotmail.com>
2020-01-21 08:45:31 -0800

    add name.sh

--- test.cpp
+++ test.cpp
@@ -6,6 +6,13 @@ TEST(EchoTest, HelloWorld) {
     EXPECT_EQ("hello world", echo(3,test_val));
 }
 
+
+TEST(EchoTest, YinjieXie) {
+    char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "Yinjie"; test_val[2] = "Xie";
+    EXPECT_EQ("Yinjie Xie", echo(3,test_val));
+}
+
+
 TEST(EchoTest, EmptyString) {
     char* test_val[1]; test_val[0] = "./c-echo";
     EXPECT_EQ("", echo(1,test_val));
